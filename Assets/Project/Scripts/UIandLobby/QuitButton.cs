80b518f546db7f5e755e9ae9f40b515eb75a1ece
Raghav Patpatia <raghav.patpatia@gmail.com>
2023-07-06 22:58:15 +0530

    Snake Game
    Edited Lobby Scene

---
+++ Assets/Project/Scripts/UIandLobby/QuitButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class QuitButton : MonoBehaviour
+{
+    private Button button;
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        PlayerPrefs.DeleteAll();
+#else
+        Application.Quit();
+#endif
+
+    }
+}
